3d8518c7a43f7791f180a48ca39e2f17e07607a0
Neel Patni <neelpatni14@gmail.com>
2025-10-18 13:49:32 +0530

    Initial commit - latest

--- server.js
+++ server.js
@@ -58,9 +58,29 @@ dotenv.config();
 const app = express();
 
 // Enable CORS for your frontend
+const cors = require('cors');
+
+const allowedOrigins = [
+    'http://mcorephysio.co.uk',
+    'https://mcorephysio.co.uk',
+    'http://www.mcorephysio.co.uk',
+    'https://www.mcorephysio.co.uk',
+    'http://127.0.0.1:5500' // optional: for local testing
+];
+
 app.use(cors({
-    origin: 'http://127.0.0.1:5500', // Matches the client's origin
+    origin: function(origin, callback){
+        // allow requests with no origin like mobile apps or curl
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1){
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
+    credentials: true
 }));
+
 
 // Parse JSON body
 app.use(express.json());
